46b8356de35595768ca562ca7d5426031322a86e
YB-SM75 <42952888+yybeltranr@users.noreply.github.com>
2023-05-16 00:59:02 -0500

    AsciiArt

---
+++ content/docs/shortcodes/segundoTaller/asciiArt.md
@@ -0,0 +1,16 @@
+# ASCII ART
+
+
+<div style="
+    background-color: #000;
+    color: #FFF;
+    font-family: 'Fira Code';
+    font-size: 1em;
+    line-height: 0.8em;
+    display: block;">
+
+{{< p5-iframe src="index.html" onload="resize(this);" sketch="/showcase/sketches/asciiArt/sketch.js" width="725" height="525" >}}
+
+
+</div>
+
---
+++ content/sketches/asciiArt/sketch.js
@@ -0,0 +1,31 @@
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ "; 
+let f;
+
+function preload () {
+  f = loadImage('/showcase/sketches/asciiArt/assets/panda.jpg');
+}
+
+function setup() {
+  noCanvas();
+  f.loadPixels();
+  
+  for(let j = 0; j < f.height; j++){
+    let row = "";
+    for(let i = 0; i < f.width; i++){
+      const pixelIndex = (i + j * f.width) * 4;
+      const r = f.pixels[pixelIndex];
+      const g = f.pixels[pixelIndex + 1];
+      const b = f.pixels[pixelIndex + 2];
+      const avg = (r+ g + b) / 3;
+      const len = density.length;
+      const charIndex = floor (map(avg, 0,255,len,0));
+      const charToShow = density.split("")[charIndex];
+      row += (charToShow === " " || charToShow === undefined) ? "&nbsp;" : charToShow;    
+    }
+    createDiv(row);
+  }
+}
+
+function draw() {
+  background(51);
+}
