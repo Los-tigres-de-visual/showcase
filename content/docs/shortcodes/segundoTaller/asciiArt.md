f64e0d4f1235263ea5c38f48d181be3103eee380
YB-SM75 <42952888+yybeltranr@users.noreply.github.com>
2023-05-24 23:02:34 -0500

    Updates ascii art - iframe completed

--- content/docs/shortcodes/segundoTaller/asciiArt.md
+++ content/docs/shortcodes/segundoTaller/asciiArt.md
@@ -1,16 +1,6 @@
 # ASCII ART
 
 
-<div style="
-    background-color: #000;
-    color: #FFF;
-    font-family: 'Fira Code';
-    font-size: 1em;
-    line-height: 0.8em;
-    display: block;">
+{{< p5-iframe sketch="/showcase/sketches/asciiArt/sketch.js"  width="520" height="620" >}}
 
-{{< p5-iframe src="index.html" onload="resize(this);" sketch="/showcase/sketches/asciiArt/sketch.js" lib1="/showcase/sketches/Depth/style.css" width="725" height="525" >}}
-
-
-</div>
 
--- content/sketches/asciiArt/sketch.js
+++ content/sketches/asciiArt/sketch.js
@@ -1,31 +1,78 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._ "; 
-let f;
+let sourceText;
+let poem;
+let gloria;
+let startIndex = 0;
+let frameRateEnabled = false;
 
-function preload () {
-  f = loadImage('/showcase/sketches/asciiArt/assets/panda.jpg');
-}
-
-function setup() {
-  noCanvas();
-  f.loadPixels();
-  
-  for(let j = 0; j < f.height; j++){
-    let row = "";
-    for(let i = 0; i < f.width; i++){
-      const pixelIndex = (i + j * f.width) * 4;
-      const r = f.pixels[pixelIndex];
-      const g = f.pixels[pixelIndex + 1];
-      const b = f.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0,255,len,0));
-      const charToShow = density.split("")[charIndex];
-      row += (charToShow === " " || charToShow === undefined) ? "&nbsp;" : charToShow;    
-    }
-    createDiv(row);
+function toggleFrameRate() {
+  frameRateEnabled = !frameRateEnabled;
+  if (frameRateEnabled) {
+    frameRate(10);
+  } else {
+    frameRate();
   }
 }
 
+function preload() {
+  gloria = loadImage("/showcase/sketches/asciiArt/assets/cat2.jpg");
+  gloria2 = loadImage("/showcase/sketches/asciiArt/assets/cat.jpg");
+  sourceText = loadStrings("/showcase/sketches/asciiArt/assets/image.txt");
+  fontRegular = loadFont('/showcase/sketches/asciiArt/assets/FiraCode-Regular.ttf');
+}
+
+function setup() {
+  createCanvas(500, 550); 
+  poem = sourceText.join(' ');
+  textFont(fontRegular);
+  let toggleButton = createButton('--> Activate ASCII Art <--');
+  toggleButton.position(width/5,height + 20);
+  toggleButton.style('padding', '8px');
+  toggleButton.style('border-radius', '15px');
+  toggleButton.style('background-color', '#D7BDE2');
+  toggleButton.style('color', '#A569BD ');
+  toggleButton.style('font-size', '20px');
+  toggleButton.style('font-family', fontRegular);
+  toggleButton.style('font-weight', 'bold');
+  toggleButton.style('letter-spacing', '4px');
+  toggleButton.style('text-shadow', '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black'); 
+  
+  toggleButton.mousePressed(toggleFrameRate);
+}
+
 function draw() {
-  background(51);
+  background(0);
+
+  
+
+  if (frameRateEnabled) {
+    let charIndex = startIndex;
+    let w = width / gloria.width;
+    let h = height / gloria.height;
+    gloria.loadPixels();
+    for (let j = 0; j < gloria.height; j++) {
+      for (let i = 0; i < gloria.width; i++) {
+        const pixelIndex = (i + j * gloria.width) * 4;
+        const r = gloria.pixels[pixelIndex + 0];
+        const g = gloria.pixels[pixelIndex + 1];
+        const b = gloria.pixels[pixelIndex + 2];
+        const avg = (r + g + b) / 3;
+        
+        noStroke();
+        fill(avg);
+        const lenp = poem.length; 
+        textSize(w * 1.4);
+        textAlign(CENTER, CENTER);
+        text(poem.charAt(charIndex % lenp), i * w + w * 0.5, j * h + h * 0.5);
+        charIndex++;
+      }
+    }
+    startIndex++;
+  } else {
+    image(gloria2, 0, 0, width, height);
+  }
+
+
+
+  
+  
 }
